92153641d122fafc0ecedd7ab2f4880a0b6d4db6
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-09 22:47:49 +0700

    formatting the output

--- search.rb
+++ search.rb
@@ -1,12 +1,27 @@
+# use rest_client gem to perform Bing search
+
 require 'rest-client'
 
-def print_data
-  url = 'https://www.bing.com'
-  query = 'microverse'
-  data = RestClient.get(url, {:params => {:q => query}})
-  puts "\nHeader:\n#{data.headers}"
-  puts "\nHeader:\n#{data.cookies}"
-  puts "\nHeader:\n#{data.body}"
+class Search
+  def initialize(url, query)
+    @data = RestClient.get(url, {:params => {:q => query}})
+  end
+
+
+
+  def print_links
+    raw = @data.body.split('b_algo"><h2><a href="').drop(1)
+    links = []
+    raw.each do |e|
+      temp = e.split('" ')
+      links << temp[0]
+    end
+    links.each {|l| puts l}
+  end
 end
 
-print_data
+url = 'https://www.bing.com/search'
+query = 'lake victoria'
+search = Search.new(url, query)
+
+search.print_links
